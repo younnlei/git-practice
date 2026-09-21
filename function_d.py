2e6b6659f16ba4196a41ab62c3042d3787ed5551
porselvi <porselvi8989@gmail.com>
2026-09-21 10:37:02 -0700

    added functiond

--- function_d.py
+++ function_d.py
@@ -2,8 +2,11 @@ def max_value(numbers):
     """ This function returns the largest number
         in the list.
     """
-    pass
-
+    max_num = 0
+    for num in numbers:
+        if num > max_num:
+            max_num =num
+    return max_num
 
 if __name__ == "__main__":
     print(max_value([1, 12, 2, 42, 8, 3]))
